c6142943a5879b13b48f9565d5539cd0062672bb
Fightingcarp <122738768+Fightingcarp@users.noreply.github.com>
2024-08-20 18:54:57 +0800

    Polished menu with sounds
    also connected it to test_level scene

--- TitleScreen/how_to.gd
+++ TitleScreen/how_to.gd
@@ -1,4 +1,5 @@
 extends Control
 
 func _on_back_pressed():
+	await get_tree().create_timer(0.3).timeout
 	get_tree().change_scene_to_file("res://TitleScreen/menu2.tscn")
--- TitleScreen/menu2.gd
+++ TitleScreen/menu2.gd
@@ -1,8 +1,10 @@
 extends Control
 
 
-func _on_start_pressed():
+func _on_start_button_pressed():
+	await get_tree().create_timer(0.3).timeout
 	get_tree().change_scene_to_file("res://TitleScreen/level_select.tscn")
 
-func _on_how_to_pressed():
+func _on_how_to_button_pressed():
+	await get_tree().create_timer(0.3).timeout
 	get_tree().change_scene_to_file("res://TitleScreen/how_to.tscn")
