39c951ee93e1ec8396b4e855195a2f40729c891e
Magnus <104272224+Magnus-Mage@users.noreply.github.com>
2024-08-20 22:18:36 +0530

    final submit for now

--- Utility/upgrade_db.gd
+++ Utility/upgrade_db.gd
@@ -6,96 +6,96 @@ const WEAPON_PATH = "res://Textures/Items/Weapons/"
 const UPGRADES = {
 	"icespear1": {
 		"icon": WEAPON_PATH + "ice_spear.png",
-		"displayname": "Ice Spear",
-		"details": "A spear of ice is thrown at a random enemy",
+		"displayname": "Injections",
+		"details": "A injection is thrown at a random enemy",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "weapon"
 	},
 	"icespear2": {
 		"icon": WEAPON_PATH + "ice_spear.png",
-		"displayname": "Ice Spear",
-		"details": "An addition Ice Spear is thrown",
+		"displayname": "Injections",
+		"details": "An addition Injection is thrown",
 		"level": "Level: 2",
 		"prerequisite": ["icespear1"],
 		"type": "weapon"
 	},
 	"icespear3": {
 		"icon": WEAPON_PATH + "ice_spear.png",
-		"displayname": "Ice Spear",
-		"details": "Ice Spears now pass through another enemy and do + 3 damage",
+		"displayname": "Injections",
+		"details": "Injection now pass through another enemy and do + 3 damage",
 		"level": "Level: 3",
 		"prerequisite": ["icespear2"],
 		"type": "weapon"
 	},
 	"icespear4": {
 		"icon": WEAPON_PATH + "ice_spear.png",
-		"displayname": "Ice Spear",
-		"details": "An additional 2 Ice Spears are thrown",
+		"displayname": "Injections",
+		"details": "An additional 2 Injections are thrown",
 		"level": "Level: 4",
 		"prerequisite": ["icespear3"],
 		"type": "weapon"
 	},
 	"javelin1": {
 		"icon": WEAPON_PATH + "javelin_3_new_attack.png",
-		"displayname": "Javelin",
-		"details": "A magical javelin will follow you attacking enemies in a straight line",
+		"displayname": "Thermometer",
+		"details": "A magical Thermometer will follow you attacking enemies in a straight line",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "weapon"
 	},
 	"javelin2": {
 		"icon": WEAPON_PATH + "javelin_3_new_attack.png",
-		"displayname": "Javelin",
-		"details": "The javelin will now attack an additional enemy per attack",
+		"displayname": "Thermometer",
+		"details": "The Thermometer will now attack an additional enemy per attack",
 		"level": "Level: 2",
 		"prerequisite": ["javelin1"],
 		"type": "weapon"
 	},
 	"javelin3": {
 		"icon": WEAPON_PATH + "javelin_3_new_attack.png",
-		"displayname": "Javelin",
-		"details": "The javelin will attack another additional enemy per attack",
+		"displayname": "Thermometer",
+		"details": "The Thermometer will attack another additional enemy per attack",
 		"level": "Level: 3",
 		"prerequisite": ["javelin2"],
 		"type": "weapon"
 	},
 	"javelin4": {
 		"icon": WEAPON_PATH + "javelin_3_new_attack.png",
-		"displayname": "Javelin",
-		"details": "The javelin now does + 5 damage per attack and causes 20% additional knockback",
+		"displayname": "Thermometer",
+		"details": "The Thermometer now does + 5 damage per attack and causes 20% additional knockback",
 		"level": "Level: 4",
 		"prerequisite": ["javelin3"],
 		"type": "weapon"
 	},
 	"tornado1": {
-		"icon": WEAPON_PATH + "tornado.png",
-		"displayname": "Tornado",
-		"details": "A tornado is created and random heads somewhere in the players direction",
+		"icon": WEAPON_PATH + "sword.png",
+		"displayname": "Cough Syrup",
+		"details": "A Cough Syrup is created and random heads somewhere in the players direction",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "weapon"
 	},
 	"tornado2": {
-		"icon": WEAPON_PATH + "tornado.png",
-		"displayname": "Tornado",
-		"details": "An additional Tornado is created",
+		"icon": WEAPON_PATH + "sword.png",
+		"displayname": "Cough Syrup",
+		"details": "An additional Cough Syrup is created",
 		"level": "Level: 2",
 		"prerequisite": ["tornado1"],
 		"type": "weapon"
 	},
 	"tornado3": {
-		"icon": WEAPON_PATH + "tornado.png",
-		"displayname": "Tornado",
-		"details": "The Tornado cooldown is reduced by 0.5 seconds",
+		"icon": WEAPON_PATH + "sword.png",
+		"displayname": "Cough Syrup",
+		"details": "The Cough Syrup cooldown is reduced by 0.5 seconds",
 		"level": "Level: 3",
 		"prerequisite": ["tornado2"],
 		"type": "weapon"
 	},
 	"tornado4": {
-		"icon": WEAPON_PATH + "tornado.png",
+		"icon": WEAPON_PATH + "sword.png",
 		"displayname": "Tornado",
-		"details": "An additional tornado is created and the knockback is increased by 25%",
+		"details": "An additional Cough Syrup is created and the knockback is increased by 25%",
 		"level": "Level: 4",
 		"prerequisite": ["tornado3"],
 		"type": "weapon"
